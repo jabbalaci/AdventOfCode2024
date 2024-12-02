b6b1b10fbb58b42a1e00fc2fd4582d986ab414f3
Jabba Laci <jabba.laci@gmail.com>
2024-12-02 06:35:25 +0100

    Day 2

---
+++ day02/part2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import helper
+
+
+def valid(numbers: list[int]) -> bool:
+    for i in range(len(numbers) - 1):
+        diff = abs(numbers[i] - numbers[i + 1])
+        if (diff < 1) or (diff > 3):
+            return False
+        #
+    #
+    return True
+
+
+def safe(original: list[int]) -> bool:
+    for i in range(len(original)):
+        numbers = original.copy()
+        del numbers[i]
+        if (numbers == sorted(numbers)) or (numbers == sorted(numbers, reverse=True)):
+            if valid(numbers):
+                return True
+            #
+        #
+    #
+    return False
+
+
+def main() -> None:
+    # lines: list[str] = helper.read_lines("example.txt")
+    lines: list[str] = helper.read_lines("input.txt")
+
+    cnt = 0
+    for line in lines:
+        numbers = [int(n) for n in line.split()]
+        if safe(numbers):
+            cnt += 1
+        #
+    #
+    print(cnt)
+
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
